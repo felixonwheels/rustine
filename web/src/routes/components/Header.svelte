b861f2c7d3a75f74e01b2b267ccd64913b578fd1
Félix Defrance <felixgif@gmail.com>
2024-08-12 23:03:21 +0200

    removes nunito font #sobrieteNumerique

--- web/src/routes/components/Header.svelte
+++ web/src/routes/components/Header.svelte
@@ -2,7 +2,7 @@
 	import RustineLogo from '$lib/assets/rustine_logo.svg?raw';
 </script>
 
-<header class="p-3">
+<header class="px-3 py-4">
 	<div class="container">
 		<div
 			class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"
@@ -11,14 +11,10 @@
 				{@html RustineLogo}
 			</a>
 
-			<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+			<ul class="nav col-12 col-lg-auto me-lg-auto ms-lg-2 mb-2 justify-content-center mb-md-0">
 				<li><a href="/" class="nav-link px-2 text-secondary">Home</a></li>
 				<li><a href="/about" class="nav-link px-2 text-secondary">About</a></li>
 			</ul>
-
-			<form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-				<input type="search" class="form-control" placeholder="Search..." aria-label="Search" />
-			</form>
 		</div>
 	</div>
 </header>
