9064312c34a705563f61acbe0a6e8f58b5d48ef2
félix <felix.defrance@mnhn.fr>
2024-08-13 11:42:09 +0200

    cors issuueeeees 🥹

--- web/src/routes/+page.ts
+++ web/src/routes/+page.ts
@@ -2,17 +2,28 @@
 // it so that it gets served as a static asset in production
 // export const prerender = true;
 
-import getDirectusInstance from '$lib/directus';
-import { readItems } from '@directus/sdk';
+// import getDirectusInstance from '$lib/directus';
+// import { readItems } from '@directus/sdk';
 
-export async function load({ fetch }) {
-	console.log(
-		'☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️ 1 ☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️'
-	);
+// export async function load({ fetch }) {
+// 	console.log(
+// 		'☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️ 1 ☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️'
+// 	);
 
-	const directus = getDirectusInstance(fetch);
+// 	const directus = getDirectusInstance(fetch);
 
-	return {
-		global: await directus.request(readItems('Global'))
-	};
-}
+// 	return {
+// 		global: await directus.request(readItems('Global'))
+// 	};
+// }
+
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch }) => {
+	const res = await fetch(`http://localhost:8055/items/Global`);
+	const item = await res.json();
+
+	console.log(item);
+
+	return { item };
+};
