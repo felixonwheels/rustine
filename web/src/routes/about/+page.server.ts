4b2a8c743e8a80fd6fcd7f09a84e06d082efd7df
Félix Defrance <felixgif@gmail.com>
2024-08-23 13:09:15 +0200

    adds about table to directus

---
+++ web/src/routes/about/+page.server.ts
@@ -0,0 +1,15 @@
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in production
+// export const prerender = true;
+
+import type { PageLoad } from './$types';
+import getDirectusInstance from '$lib/directus';
+import { readItems } from '@directus/sdk';
+
+export const load: PageLoad = async ({ fetch }) => {
+	const directus = getDirectusInstance(fetch);
+
+	return {
+		about: await directus.request(readItems('About'))
+	};
+};
