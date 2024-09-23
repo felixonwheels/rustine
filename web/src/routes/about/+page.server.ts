1f114dd461e316767a857cc0e26a9447a1ac34fc
omni <felixgif@gmail.com>
2024-09-23 23:04:19 +0200

    ci test 3

--- web/src/routes/about/+page.server.ts
+++ web/src/routes/about/+page.server.ts
@@ -2,7 +2,9 @@ import type { PageServerLoad } from './$types';
 import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch, depends }) => {
+	depends('paraglide:lang');
+
 	const directus = getDirectusInstance(fetch);
 
 	return {
