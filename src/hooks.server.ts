f957b6423e742c4e186ee27ccca4a2933be9fe9c
félix <felix.defrance@mnhn.fr>
2024-10-07 15:39:34 +0200

    fixes server hooks issue

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,12 @@
-import type { Handle } from '@sveltejs/kit';
-
 import { i18n } from '$lib/i18n';
 import { sequence } from '@sveltejs/kit/hooks';
 
-const directusHeaders: Handle = async ({ event, resolve }) => {
+async function directusHeaders({ event, resolve }) {
 	return await resolve(event, {
 		filterSerializedResponseHeaders: (key, _) => {
 			return key.toLowerCase() === 'content-type';
 		}
 	});
-};
+}
 
 export const handle = sequence(i18n.handle(), directusHeaders);
