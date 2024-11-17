5dd22e7ec7bc12ea27071e4a45cc7bc3ca43506a
omni <felixgif@gmail.com>
2024-11-17 23:53:16 +0100

    improves landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-	import RustineTitle from '$lib/components/RustineTitle.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
 	import * as m from '$lib/paraglide/messages.js';
-	import { BookOpenText, CircleHelp, Wrench } from 'lucide-svelte';
-	import { fly } from 'svelte/transition';
-
-	const sidebar = useSidebar();
+	import { Wrench } from 'lucide-svelte';
 
 	import type { PageData } from './$types';
 
@@ -18,50 +13,42 @@
 	<meta content="Rustine" name="description" />
 </svelte:head>
 
-<section>
-	<div>
-		{#if sidebar.isMobile}
-			<div>
-				<RustineTitle />
+<h1 class=" text-center font-['Roboto'] text-5xl font-bold italic md:text-6xl xl:text-7xl">
+	{m.titleStart()}<mark
+		class="bg-gradient-to-r from-rustine-orange to-rustine-indigo bg-clip-text not-italic text-transparent"
+		>{m.titleMark()}</mark
+	>{m.titleEnd()}
+</h1>
+
+<div class="mt-6">
+	{@html data.global?.description}
+</div>
+
+<div class="mt-6 flex flex-col justify-center gap-3 sm:flex-row">
+	<Button class="flex items-center justify-center gap-1" href="/learn" variant="outline"
+		>{m.learnButton()}</Button
+	>
+	<Button class="flex items-center justify-center gap-1" href="/fix">{m.fixButton()}</Button>
+</div>
+
+<div class="mt-16">
+	<h2 class="text-4xl font-bold lg:text-5xl lg:tracking-tight">
+		Everything you need to fix your bike
+	</h2>
+	<p class="mt-4 text-lg text-slate-600">a very convincing description</p>
+</div>
+
+<div class="mt-16 grid gap-16 sm:grid-cols-2 xl:grid-cols-3">
+	{#each [...Array(8).keys()] as _, i}
+		<div class="flex items-start gap-4">
+			<div class="mt-1 h-8 w-8 shrink-0 rounded-full bg-black p-2">
+				<Wrench />
 			</div>
-		{/if}
-
-		<div>
-			{@html data.global?.title}
+			<div>
+				<h3 class="text-lg font-semibold">{m.titleMark()}</h3>
+				{' '}
+				<p class="mt-2 leading-relaxed text-slate-500">{@html data.global?.description}</p>
+			</div>
 		</div>
-
-		<div>
-			{@html data.global?.description}
-		</div>
-
-		<div>
-			<Button href="/learn">learn about your bike</Button>
-			<Button href="/fix">fix your bike!</Button>
-		</div>
-	</div>
-</section>
-
-<section>
-	<div>
-		<article in:fly={{ duration: 120 }}>
-			<h1><BookOpenText color="var(--pico-primary)" size={28} /></h1>
-			<h2>{m.learn()}</h2>
-			<p>learn about your bike, its components and how they all work together</p>
-			<p><a href="/learn">learn more</a></p>
-		</article>
-
-		<article in:fly={{ duration: 120 }}>
-			<h1><Wrench color="var(--pico-primary)" size={28} /></h1>
-			<h2>{m.fix()}</h2>
-			<p>diagnose an issue on your bike, and learn how to fix it</p>
-			<p><a href="/fix">fix my bike</a></p>
-		</article>
-
-		<article in:fly={{ duration: 120 }}>
-			<h1><CircleHelp color="var(--pico-primary)" size={28} /></h1>
-			<h2>{m.about()}</h2>
-			<p>read about what this project is and why it came to be</p>
-			<p><a href="/about">what even is this?</a></p>
-		</article>
-	</div>
-</section>
+	{/each}
+</div>
