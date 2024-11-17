c0c267d9ca66efa7dc4d9b63b1c895776e021e27
Félix Defrance <felixgif@gmail.com>
2024-11-17 14:52:27 +0100

    adds wip banner

---
+++ src/lib/components/WIPBanner.svelte
@@ -0,0 +1,10 @@
+<div
+	class="flex place-content-center gap-x-6 overflow-hidden bg-gradient-to-r from-rustine-orange/50 to-rustine-indigo/50 p-2 text-center md:rounded-t-xl"
+>
+	<div
+		class="absolute w-full bg-gradient-to-r from-rustine-orange to-rustine-indigo opacity-30"
+	></div>
+	<div class="text-sm/6 opacity-100">
+		⚠️ This is a work in progress. It doesn't do much for now, I'm just noodling around 🍜
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import WIPBanner from '$lib/components/WIPBanner.svelte';
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { ModeWatcher } from 'mode-watcher';
@@ -20,6 +21,8 @@
 		<AppSidebar />
 
 		<Sidebar.Inset>
+			<WIPBanner />
+
 			<header class="flex h-16 shrink-0 items-center gap-2">
 				<div class="flex items-center gap-2 px-4">
 					<Sidebar.Trigger class="-ml-1" />
