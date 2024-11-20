a757804eeef961d245e4ca23f7dcbfe63f737225
omni <felixgif@gmail.com>
2024-11-20 22:47:14 +0100

    translates WIP banner

--- src/lib/components/WIPBanner.svelte
+++ src/lib/components/WIPBanner.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages.js';
+</script>
+
 <div
 	class="flex place-content-center bg-destructive p-2 text-center text-destructive-foreground shadow-md md:rounded-t-xl"
 >
 	<div class="font-bold">
-		⚠️ This is a work in progress. It doesn't do much for now, I'm just noodling around 🍜
+		{m.wip()}
 	</div>
 </div>
